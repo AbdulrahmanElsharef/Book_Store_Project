cc4586aa7d4d5f73a69ee5715f816e5a59f88ce2
abdo elsharef <abdoelsharef1@gmail.com>
2023-01-10 00:24:42 +0200

    update

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your models here.
+class Book(models.Model):
+    author=models.CharField(max_length=25)
+    title=models.CharField(max_length=50)
+    details=models.TextField(max_length=500)
+    image=models.ImageField(upload_to='/books/images')
+    slug=models.SlugField(null=True,blank=True)
+    cateogry=models.ForeignKey('cateogry', on_delete=models.CASCADE,related_name='cateogry')
+    def __str__(self) :
+        return self.title
+    
+    
+class cateogry(models.Model):
+    name=models.CharField(max_length=25)
+    
+    def __str__(self) :
+        return self.name
+    
