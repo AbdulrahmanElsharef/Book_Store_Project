f80c201953b3428769ce6eff05ee5305d928c1f2
unknown <abdoelsharef1@gmail.com>
2023-01-10 23:28:25 +0200

    create models

--- liprary/views.py
+++ liprary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book,cateogry
+
+
+def all_books(request):
+    all=Book.objects.all()
+    context={'books':all}
+    return render (request,'books.html',context)
