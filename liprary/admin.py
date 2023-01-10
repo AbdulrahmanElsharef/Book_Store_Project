d3a9b95d8a151de4edf73501910fde42cb3f17b0
abdo elsharef <abdoelsharef1@gmail.com>
2023-01-10 02:09:26 +0200

    update view

--- liprary/admin.py
+++ liprary/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Book , cateogry
+
+
+class lipraryAdmin(admin.ModelAdmin):
+    list_display=['title','author']
+    list_filter=['cateogry','author']
+    search_fields=['details','title']
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book,lipraryAdmin)
 admin.site.register(cateogry)
--- liprary/models.py
+++ liprary/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 # Create your models here.
+
 class Book(models.Model):
-    author=models.CharField(max_length=25)
     title=models.CharField(max_length=50)
+    author=models.CharField(max_length=25)
     details=models.TextField(max_length=500)
     slug=models.SlugField(null=True,blank=True)
     cateogry=models.ForeignKey('cateogry', on_delete=models.CASCADE,related_name='cateogry')
     image=models.ImageField(upload_to='liprary/images')
     def __str__(self) :
         return self.title
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Book, self).save(*args, **kwargs)
     
     
 class cateogry(models.Model):
